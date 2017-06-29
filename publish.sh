2454c9363dea1e81abcf9556a37dc0ddd489cd8e
pvpgit <parul00@gmail.com>
2017-06-28 23:24:53 -0400

    Update

---
+++ publish.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+jupyter-nbconvert --to slides Myslides.ipynb --reveal-prefix=reveal.js
+mv Myslides.slides.html  index.html
+mkdir -p /tmp/workspace
+cp -r * /tmp/workspace/
+git add -A .
+git commit -m "Update"
+git checkout -B gh-pages
+cp -r /tmp/workspace/* .
+git add -A .
+git commit -m "new version"
+git push origin master gh-pages
+git checkout master
+rm -rf /tmp/workspace
+
